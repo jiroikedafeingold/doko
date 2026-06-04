037dca0ec3ee976a9620dd8196bb11e1703e9d98
Jiro Feingold <jiro@feingold5.com>
2026-06-04 11:51:12 -0700

    Initial Commit

---
+++ ShizzNeeded/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShizzNeeded
+//
+//  Created by Jiro on 6/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShizzNeeded/ShizzNeededApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShizzNeededApp.swift
+//  ShizzNeeded
+//
+//  Created by Jiro on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ShizzNeededApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
